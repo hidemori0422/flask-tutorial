63828971ace84f3f6b13b5539f577b9e6dbdee71
hidmor01 <hideki.mori@arm.com>
2020-05-02 23:20:29 +0100

    Finish: The Flask Mega-Tutorial Part II: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,25 @@
+from flask import render_template
+
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/second_page')
+def second_page():
+    return render_template('second_page.html')
