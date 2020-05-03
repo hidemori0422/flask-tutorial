08b0b895ced78b15c116b15ee2f1af11c35f82dc
hidmor01 <hideki.mori@arm.com>
2020-05-03 21:43:56 +0100

    Remove bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# Bootstrap(app)
 
 from app import routes
